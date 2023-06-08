b997d85e9ae4df4cc42ebf42f399e235e5aeb300
franneck94 <schaffranek.jan@web.de>
2023-06-08 13:32:05 +0200

    up

--- 2_ImGui/Desktop_Own/src/render.cpp
+++ 2_ImGui/Desktop_Own/src/render.cpp
@@ -20,10 +20,68 @@ void WindowClass::Draw(std::string_view label)
 
     ImGui::Begin(label.data(), nullptr, window_flags);
 
+    DrawDesktop();
+    DrawTaskbar();
 
     ImGui::End();
 }
 
+void WindowClass::DrawDesktop()
+{
+    for (auto &icon : icons)
+        icon.Draw();
+}
+
+void WindowClass::DrawTaskbar()
+{
+    constexpr static auto taskbar_flags =
+        ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar |
+        ImGuiWindowFlags_NoTitleBar;
+    constexpr static auto taskbar_size = ImVec2(1280.0F, 40.0F);
+    constexpr static auto taskbar_pos = ImVec2(0.0F, 680.0F);
+
+    static auto open_taskbar = false;
+
+    ImGui::SetNextWindowSize(taskbar_size);
+    ImGui::SetNextWindowPos(taskbar_pos);
+
+    ImGui::Begin("Taskbar", nullptr, taskbar_flags);
+
+    if (ImGui::Button("All Icons"))
+    {
+        ImGui::OpenPopup("My Programs");
+        open_taskbar = true;
+    }
+
+    if (open_taskbar)
+        ShowIconList();
+
+    ImGui::SameLine();
+
+    ImGui::SetCursorPosX(ImGui::GetContentRegionAvail().x);
+
+    static auto clock_open = false;
+    clock.GetTime();
+    const auto time = fmt::format("{}:{}", clock.hrs, clock.mins);
+    if (ImGui::Button(time.data(), ImVec2(100.0F, 30.0F)) || clock_open)
+    {
+        clock.Draw("clockWindow");
+        clock_open = true;
+    }
+    if (clock_open && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
+        clock_open = false;
+
+    ImGui::End();
+}
+
+void WindowClass::ShowIconList(bool *open)
+{
+}
+
+void WindowClass::Icon::Draw()
+{
+}
 
 void render(WindowClass &window_class)
 {
--- 2_ImGui/Desktop_Own/src/render.hpp
+++ 2_ImGui/Desktop_Own/src/render.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 #include <string_view>
+#include <vector>
 
 #include <fmt/format.h>
 #include <imgui.h>
@@ -12,9 +14,40 @@
 class WindowClass
 {
 public:
+    constexpr static auto numIcons = std::uint32_t{10};
+
+    struct Icon
+    {
+        Icon(std::string_view label_)
+            : label(label_), position(ImVec2{}), popupOpen(false),
+              clickCount(0){};
+        void Draw();
+
+        std::string label;
+        ImVec2 position;
+        bool popupOpen;
+        std::uint32_t clickCount;
+    };
+
+public:
+    WindowClass() : icons(), clock({})
+    {
+        icons.reserve(numIcons);
+        for (std::uint32_t i = 0; i < numIcons; ++i)
+            icons.push_back(Icon{fmt::format("Icon{}", i)});
+    };
+
     void Draw(std::string_view label);
 
 private:
+    void DrawDesktop();
+    void DrawTaskbar();
+
+    void ShowIconList(bool *open = nullptr);
+
+private:
+    std::vector<Icon> icons;
+
     Clock clock;
 };
 
