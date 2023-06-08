9f60013b972820b85c161f6d7fd855d7df8a7674
franneck94 <schaffranek.jan@web.de>
2023-06-08 20:43:52 +0200

    up

--- 2_ImGui/Desktop/src/render.cpp
+++ 2_ImGui/Desktop/src/render.cpp
@@ -34,17 +34,17 @@ void WindowClass::DrawDesktop()
 
 void WindowClass::DrawTaskbar()
 {
-    static constexpr auto taskbar_flags =
+    constexpr static auto taskbar_flags =
         ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
         ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar |
         ImGuiWindowFlags_NoTitleBar;
-    static constexpr auto taskbar_size = ImVec2(1280.0F, 40.0F);
-    static constexpr auto taskbar_pos = ImVec2(0.0F, 680.0F);
+    constexpr static auto taskbar_size = ImVec2(1280.0F, 40.0F);
+    constexpr static auto taskbar_pos = ImVec2(0.0F, 680.0F);
 
     static auto open_taskbar = false;
 
-    ImGui::SetNextWindowPos(taskbar_size, ImGuiCond_Always);
-    ImGui::SetNextWindowSize(taskbar_pos, ImGuiCond_Always);
+    ImGui::SetNextWindowSize(taskbar_size);
+    ImGui::SetNextWindowPos(taskbar_pos);
 
     ImGui::Begin("Taskbar", nullptr, taskbar_flags);
 
@@ -64,7 +64,7 @@ void WindowClass::DrawTaskbar()
     static auto clock_open = false;
     clock.GetTime();
     const auto time = fmt::format("{}:{}", clock.hrs, clock.mins);
-    if (ImGui::Button(time.data(), ImVec2(100, 30)) || clock_open)
+    if (ImGui::Button(time.data(), ImVec2(100.0F, 30.0F)) || clock_open)
     {
         clock.Draw("clockWindow");
         clock_open = true;
@@ -80,9 +80,9 @@ void WindowClass::ShowIconList(bool *open)
     const auto selectable_height = ImGui::GetTextLineHeightWithSpacing();
     const auto popup_height = selectable_height * numIcons + 40.0F;
 
+    ImGui::SetNextWindowSize(ImVec2(100.0F, popup_height), ImGuiCond_Always);
     ImGui::SetNextWindowPos(ImVec2(0.0F, 680.0F - popup_height),
                             ImGuiCond_Always);
-    ImGui::SetNextWindowSize(ImVec2(100.0F, popup_height), ImGuiCond_Always);
 
     if (ImGui::BeginPopupModal("My Programs", open, ImGuiWindowFlags_NoResize))
     {
@@ -101,44 +101,47 @@ void WindowClass::ShowIconList(bool *open)
 
 void WindowClass::Icon::Draw()
 {
+    constexpr static auto icon_window_flags = ImGuiWindowFlags_NoResize |
+                                              ImGuiWindowFlags_NoCollapse |
+                                              ImGuiWindowFlags_NoScrollbar;
     constexpr static auto button_size = ImVec2(100.0F, 50.0F);
-    const auto label_popup = fmt::format("Icon Popop Window##{}", label);
 
-    ImGui::SetNextWindowPos(position, ImGuiCond_FirstUseEver);
+    const auto label_icon_window = fmt::format("IconWindow##{}", label);
+    const auto label_icon_popup = fmt::format("IconPopup##{}", label);
+
     ImGui::SetNextWindowSize(
         ImVec2(button_size.x + 35.0F, button_size.y + 35.0F),
-        ImGuiCond_FirstUseEver);
-    ImGui::Begin(label.data(),
-                 nullptr,
-                 (ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar |
-                  ImGuiWindowFlags_NoResize));
+        ImGuiCond_Always);
+    ImGui::SetNextWindowPos(position, ImGuiCond_FirstUseEver);
+
+    ImGui::Begin(label_icon_window.data(), nullptr, icon_window_flags);
 
     if (ImGui::Button(label.data(), button_size))
     {
         ++clickCount;
     }
 
-    ImGui::End();
-
     if (clickCount >= 1 || popupOpen)
     {
-        ImGui::OpenPopup(label_popup.data());
+        ImGui::OpenPopup(label_icon_popup.data());
         clickCount = 0;
         popupOpen = true;
     }
 
-    if (ImGui::BeginPopupModal(label_popup.data(), &popupOpen))
+    if (ImGui::BeginPopupModal(label_icon_popup.data(), &popupOpen))
     {
         ImGui::Text("Hi");
 
         if (ImGui::Button("Close"))
         {
-            ImGui::CloseCurrentPopup();
             popupOpen = false;
+            ImGui::CloseCurrentPopup();
         }
 
         ImGui::EndPopup();
     }
+
+    ImGui::End();
 }
 
 void render(WindowClass &window_class)
