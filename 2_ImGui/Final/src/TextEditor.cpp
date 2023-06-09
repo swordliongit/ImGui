93a508f095e07dd30f085352a7098bc9cbbd12a1
franneck94 <schaffranek.jan@web.de>
2023-06-09 08:52:34 +0200

    up

--- 2_ImGui/Final/src/TextEditor.cpp
+++ 2_ImGui/Final/src/TextEditor.cpp
@@ -1,13 +1,14 @@
 #include <algorithm>
+#include <cstring>
 #include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <string_view>
 
 #include <fmt/format.h>
 #include <imgui.h>
-#include <imgui_stdlib.h>
 #include <implot.h>
 
 #include "TextEditor.hpp"
@@ -16,19 +17,6 @@ namespace fs = std::filesystem;
 
 void TextEditor::Draw(std::string_view label, bool *open)
 {
-    static constexpr auto input_flags =
-        (ImGuiInputTextFlags_AllowTabInput |
-         ImGuiInputTextFlags_NoHorizontalScroll);
-
-    static char saveFilenameBuffer[512] = {"\0"};
-    static char loadFilenameBuffer[512] = {"\0"};
-
-    const auto ctrl_pressed = ImGui::GetIO().KeyCtrl;
-    const auto esc_pressed =
-        ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape));
-    const auto s_pressed = ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_S));
-    const auto l_pressed = ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_L));
-
     ImGui::SetNextWindowPos(mainWindowPos, ImGuiCond_Always);
     ImGui::SetNextWindowSize(mainWindowSize, ImGuiCond_Always);
 
@@ -36,7 +24,20 @@ void TextEditor::Draw(std::string_view label, bool *open)
                  open,
                  mainWindowFlags | ImGuiWindowFlags_MenuBar);
 
-    SettingsMenuBar();
+    DrawMenu();
+    DrawContent();
+    DrawInfo();
+
+    ImGui::End();
+}
+
+void TextEditor::DrawMenu()
+{
+    const auto ctrl_pressed = ImGui::GetIO().KeyCtrl;
+    const auto s_pressed =
+        ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape));
+    const auto l_pressed =
+        ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape));
 
     if (ImGui::Button("Save") || (ctrl_pressed && s_pressed))
     {
@@ -50,13 +51,32 @@ void TextEditor::Draw(std::string_view label, bool *open)
         ImGui::OpenPopup("Load File");
     }
 
-    if (ImGui::BeginPopupModal("Save File"))
+    ImGui::SameLine();
+
+    if (ImGui::Button("Clear"))
+    {
+        std::memset(textBuffer, 0, bufferSize);
+    }
+
+    DrawSavePopup();
+    DrawLoadPopup();
+}
+
+void TextEditor::DrawSavePopup()
+{
+    static char saveFilenameBuffer[256] = "text.txt";
+    const auto esc_pressed =
+        ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape));
+
+    ImGui::SetNextWindowSize(popUpSize);
+    ImGui::SetNextWindowPos(popUpPos);
+    if (ImGui::BeginPopupModal("Save File", nullptr, popUpFlags))
     {
         ImGui::InputText("Filename",
                          saveFilenameBuffer,
                          sizeof(saveFilenameBuffer));
 
-        if (ImGui::Button("Save", ImVec2(120.0F, 0.0F)))
+        if (ImGui::Button("Save", popUpButtonSize))
         {
             SaveToFile(saveFilenameBuffer);
             currentFilename = saveFilenameBuffer;
@@ -65,21 +85,30 @@ void TextEditor::Draw(std::string_view label, bool *open)
 
         ImGui::SameLine();
 
-        if (ImGui::Button("Cancel", ImVec2(120.0F, 0.0F)) || esc_pressed)
+        if (ImGui::Button("Cancel", popUpButtonSize) || esc_pressed)
         {
             ImGui::CloseCurrentPopup();
         }
 
         ImGui::EndPopup();
     }
+}
 
-    if (ImGui::BeginPopupModal("Load File"))
+void TextEditor::DrawLoadPopup()
+{
+    static char loadFilenameBuffer[256] = "text.txt";
+    const auto esc_pressed =
+        ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape));
+
+    ImGui::SetNextWindowSize(popUpSize);
+    ImGui::SetNextWindowPos(popUpPos);
+    if (ImGui::BeginPopupModal("Load File", nullptr, popUpFlags))
     {
         ImGui::InputText("Filename",
                          loadFilenameBuffer,
                          sizeof(loadFilenameBuffer));
 
-        if (ImGui::Button("Load", ImVec2(120.0F, 0.0F)))
+        if (ImGui::Button("Load", popUpButtonSize))
         {
             LoadFromFile(loadFilenameBuffer);
             currentFilename = loadFilenameBuffer;
@@ -88,54 +117,59 @@ void TextEditor::Draw(std::string_view label, bool *open)
 
         ImGui::SameLine();
 
-        if (ImGui::Button("Cancel", ImVec2(120.0F, 0.0F)) || esc_pressed)
+        if (ImGui::Button("Cancel", popUpButtonSize) || esc_pressed)
         {
             ImGui::CloseCurrentPopup();
         }
 
         ImGui::EndPopup();
     }
+}
 
-    ImGui::BeginChild("LineNumbers", ImVec2(30.0F, 625.0F));
+void TextEditor::DrawContent()
+{
+    static constexpr auto inputTextSize = ImVec2(1200.0F, 625.0F);
+    static constexpr auto lineNumberSize = ImVec2(30.0F, inputTextSize.y);
+    static constexpr auto inputTextFlags =
+        ImGuiInputTextFlags_AllowTabInput |
+        ImGuiInputTextFlags_NoHorizontalScroll;
+
+    ImGui::BeginChild("LineNumbers", lineNumberSize);
 
     const auto line_count =
-        std::count(textBuffer.begin(), textBuffer.end(), '\n') + 1;
+        std::count(textBuffer, textBuffer + bufferSize, '\n') + 1;
 
     for (auto i = 1; i <= line_count; ++i)
-    {
         ImGui::Text("%d", i);
-    }
 
     ImGui::EndChild();
 
     ImGui::SameLine();
 
     ImGui::InputTextMultiline("###inputField",
-                              &textBuffer,
-                              ImVec2(1200.0F, 625.0F),
-                              input_flags);
-
-    if (currentFilename.size() > 0)
-    {
-        const auto file_extension = GetFileExtension(currentFilename);
-        ImGui::Text("Opened file %s | File Extension: %s",
-                    currentFilename.data(),
-                    file_extension.data());
-    }
-    else
-        ImGui::Text("No file opened!");
-
-    ImGui::End();
+                              textBuffer,
+                              bufferSize,
+                              inputTextSize,
+                              inputTextFlags);
 }
 
-std::string TextEditor::GetFileExtension(std::string_view filename)
+void TextEditor::DrawInfo()
 {
-    return fs::path(filename).extension().string();
+    if (currentFilename.size() == 0)
+    {
+        ImGui::Text("No File Opened!");
+        return;
+    }
+
+    const auto file_extension = GetFileExtension(currentFilename);
+    ImGui::Text("Opened file %s | File extension %s",
+                currentFilename.data(),
+                file_extension.data());
 }
 
 void TextEditor::SaveToFile(std::string_view filename)
 {
-    auto out = std::ofstream(filename.data());
+    auto out = std::ofstream{filename.data()};
 
     if (out.is_open())
     {
@@ -146,13 +180,20 @@ void TextEditor::SaveToFile(std::string_view filename)
 
 void TextEditor::LoadFromFile(std::string_view filename)
 {
-    auto in = std::ifstream(filename.data());
+    auto in = std::ifstream{filename.data()};
 
     if (in.is_open())
     {
         auto buffer = std::stringstream{};
         buffer << in.rdbuf();
-        textBuffer = buffer.str();
+        std::memcpy(textBuffer, buffer.str().data(), bufferSize);
         in.close();
     }
+}
+
+std::string TextEditor::GetFileExtension(std::string_view filename)
+{
+    const auto file_path = fs::path{filename};
+
+    return file_path.extension().string();
 }
